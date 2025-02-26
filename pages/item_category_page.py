2609a7f09d3adf581d0c49b9318b471f6338f42f
Kamil Gierczycki <kam.gierczycki@gmail.com>
2025-02-26 21:44:18 +0100

    test(cart): add laptop to cart

--- pages/item_category_page.py
+++ pages/item_category_page.py
@@ -6,6 +6,7 @@ from pages.base_page import BasePage
 class PhonesCategoryPage(BasePage):
     CLICK_PHONES_CATEGORY = (By.LINK_TEXT, 'Phones')
     CLICK_SAMSUNG_GALAXY_S6 = (By.LINK_TEXT,"Samsung galaxy s6")
+
     def click_open_phones_category(self):
         self.click(*self.CLICK_PHONES_CATEGORY)
         self.click(*self.CLICK_SAMSUNG_GALAXY_S6)
@@ -13,13 +14,17 @@ class PhonesCategoryPage(BasePage):
 
 class LaptopsCategoryPage(BasePage):
     CLICK_LAPTOPS_CATEGORY = (By.LINK_TEXT, "Laptops")
+    CLICK_SONY_VAIO_I5 = (By.LINK_TEXT, "Sony vaio i5")
 
     def click_open_laptops_category(self):
         self.click(*self.CLICK_LAPTOPS_CATEGORY)
+        self.click(*self.CLICK_SONY_VAIO_I5)
 
 
 class MonitorsCategoryPage(BasePage):
     CLICK_MONITORS_CATEGORY = (By.LINK_TEXT, "Monitors")
+    CLICK_ASUS_FULL_HD = (By.LINK_TEXT, "ASUS Full HD")
 
     def click_open_monitors_category(self):
         self.click(*self.CLICK_MONITORS_CATEGORY)
+        self.click(*self.CLICK_ASUS_FULL_HD)
--- tests/test_place_order.py
+++ tests/test_place_order.py
@@ -10,9 +10,12 @@ from pages.item_category_page import PhonesCategoryPage, LaptopsCategoryPage, Mo
 from pages.specific_item_page import AddItemToCart
 from pages import specific_item_page
 
-def login_as_valid_user(driver):
+
+@pytest.fixture
+def logged_in_page(driver):
     login_popup = LoginPopup(driver)
     login_popup.click_open_login_popup()
+
 
     WebDriverWait(driver, 10).until(EC.visibility_of_element_located(login_popup.LOGIN_BUTTON))
 
@@ -20,16 +23,28 @@ def login_as_valid_user(driver):
     login_popup.enter_password("testowehaslo41")
     login_popup.click_login()
     time.sleep(5)
+    return driver
 
-def test_add_phone_to_cart(driver):
-    login_as_valid_user(driver)
-    phones_page = PhonesCategoryPage(driver)
+def test_add_phone_to_cart(logged_in_page):
+
+    phones_page = PhonesCategoryPage(logged_in_page)
     phones_page.click_open_phones_category()
 
     time.sleep(2)
-    specific_phone_page = AddItemToCart(driver)
+    specific_phone_page = AddItemToCart(logged_in_page)
     specific_phone_page.add_item_to_cart()
     specific_phone_page.return_to_homepage()
+
+    laptops_page = LaptopsCategoryPage(logged_in_page)
+    time.sleep(2)
+    laptops_page.click_open_laptops_category()
+
+    time.sleep(2)
+    specific_laptop_page = AddItemToCart(logged_in_page)
+    specific_laptop_page.add_item_to_cart()
+    specific_laptop_page.return_to_homepage()
+
+
 
     #specific_item_page.AddItemToCart(driver)
 
